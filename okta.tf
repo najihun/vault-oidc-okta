05b5489d8870b1d308093f522d5e534b2cc9e411
najihun <jihun.na@hashicorp.com>
2022-07-16 16:16:34 +0900

    test oidc

--- okta.tf
+++ okta.tf
@@ -13,10 +13,10 @@ resource "vault_jwt_auth_backend_role" "okta_role" {
         var.okta_redirect_uris,
         "http://localhost:8250/oidc/callback"
     ]
-    
+
     role_type       = "oidc"
     user_claim = "sub"
-    token_policies = ["default"]
+//   token_policies = ["default"]
 }
 
 
