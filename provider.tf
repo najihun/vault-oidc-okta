bb47cb43cab86d19608d89034e1c331fe6165bb7
najihun <jihun.na@hashicorp.com>
2022-07-16 15:11:32 +0900

    okta provider

--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
       source = "hashicorp/vault"
       version = "3.7.0"
     }
+
+    okta = {
+      source = "okta/okta"
+      version = "3.31.0"
+    }
   }
 }
 
@@ -13,4 +18,8 @@ provider "vault" {
   namespace = "admin"
 }
 
-
+provider "okta" {
+  org_name = var.okta_org_name
+  base_url = var.okta_base_url
+  api_token = var.okta_token
+}
