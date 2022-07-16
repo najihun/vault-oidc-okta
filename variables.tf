f89e93c24a46357d3f0c8910f76d688d5d02d70a
najihun <jihun.na@hashicorp.com>
2022-07-16 15:48:08 +0900

    test oidc

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
 variable "admin_password" {
   type = string
 }
+
+variable "okta_mount_path" {
+  type = string
+  default = "okta"
+}
+
+variable "okta_discovery_url"{
+  type = string
+}
+
+variable "okta_redirect_uris"{
+  type = string
+}
