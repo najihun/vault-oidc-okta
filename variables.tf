496b67f164e873bd06840039c14752d61809deb3
najihun <jihun.na@hashicorp.com>
2022-07-16 23:33:41 +0900

    test okta

--- variables.tf
+++ variables.tf
@@ -33,15 +33,3 @@ variable "okta_client_secret" {
   type        = string
   description = "okta client secret"
 }
-
-variable "okta_org_name" {
-  type = string
-}
-
-variable "okta_base_url" {
-  type = string
-}
-
-variable "okta_api_token" {
-  type = string
-}
