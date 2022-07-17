c23a34184b3a3ae674ecd46bd5e237e9c7e993ea
najihun <jihun.na@hashicorp.com>
2022-07-17 19:46:32 +0900

    groups

--- groups.tf
+++ groups.tf
@@ -11,7 +11,7 @@ resource "vault_identity_group" "okta-group-vault-developer" {
 
 // external group alias for okta developers
 resource "vault_identity_group_alias" "okta-group-vault-developer-alias" {
-  name           = "okta-group-vault-developers"
+  name           = "okta-group-vault-developer"
   mount_accessor = vault_jwt_auth_backend.okta_oidc.accessor
   canonical_id   = vault_identity_group.okta-group-vault-developer.id
 }
