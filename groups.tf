6238c5251a45bcfb84fd395156f30f3cce4888bd
najihun <jihun.na@hashicorp.com>
2022-07-17 17:54:42 +0900

    added groups and groups-alias for developers

--- groups.tf
+++ groups.tf
@@ -8,3 +8,10 @@ resource "vault_identity_group" "okta-group-vault-developer" {
     responsibility = "okta-group-vault-developer"
   }
 }
+
+// external group alias for okta developers
+resource "vault_identity_group_alias" "okta-group-vault-developer-alias" {
+  name           = "okta-group-vault-developers"
+  mount_accessor = vault_jwt_auth_backend.okta_oidc.accessor
+  canonical_id   = vault_identity_group.okta-group-vault-developer.id
+}
