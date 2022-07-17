d9a7bb0b290525ce8c61399d61e4415c16a2a68e
najihun <jihun.na@hashicorp.com>
2022-07-17 22:45:59 +0900

    vault-admin-okta

--- oidc_okta.tf
+++ oidc_okta.tf
@@ -5,9 +5,6 @@ resource "vault_jwt_auth_backend" "okta_oidc" {
   oidc_client_id   = var.okta_client_id
   oidc_client_secret = var.okta_client_secret
   oidc_discovery_url = var.okta_discovery_url
-  tune = {
-    listing_visibility = "unauth"
-  }
 }
 
 // test role to verify integration with Okta
