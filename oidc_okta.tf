e21ba523053a8b884a19c9ed005c31ea2249017b
najihun <jihun.na@hashicorp.com>
2022-07-17 21:42:36 +0900

    edited jwt_backennd

--- oidc_okta.tf
+++ oidc_okta.tf
@@ -1,9 +1,8 @@
 // auth_method: oidc path:okta
 resource "vault_jwt_auth_backend" "okta_oidc" {
-  description        = "Okta OIDC"
   path               = var.okta_mount_path
   type               = "oidc"
-  oidc_client_id = var.okta_client_id
+  oidc_client_id     = var.okta_client_id
   oidc_client_secret = var.okta_client_secret
   oidc_discovery_url = var.okta_discovery_url
   bound_issuer       = var.okta_discovery_url
@@ -11,31 +10,31 @@ resource "vault_jwt_auth_backend" "okta_oidc" {
 
 // test role to verify integration with Okta
 resource "vault_jwt_auth_backend_role" "okta_role" {
-    backend = vault_jwt_auth_backend.okta_oidc.path
-    bound_audiences = [ var.okta_client_id ]
-    role_name = "vault-role-okta-default"
-    allowed_redirect_uris = [
-        var.okta_redirect_uris,
-        "http://localhost:8250/oidc/callback",
-    ]
-    role_type       = "oidc"
-    user_claim = "sub"
-    token_policies = [ "default" ]
+  backend         = vault_jwt_auth_backend.okta_oidc.path
+  bound_audiences = [var.okta_client_id]
+  role_name       = "vault-role-okta-default"
+  allowed_redirect_uris = [
+    var.okta_redirect_uris,
+    "http://localhost:8250/oidc/callback",
+  ]
+  role_type      = "oidc"
+  user_claim     = "sub"
+  token_policies = ["default"]
 }
 
 // developer role
 resource "vault_jwt_auth_backend_role" "vault-role-okta-group-vault-developer" {
-    backend = vault_jwt_auth_backend.okta_oidc.path
-    bound_audiences = [ var.okta_client_id ]
-    role_name = "vault-role-okta-group-vault-developer"
-    allowed_redirect_uris = [
-        var.okta_redirect_uris,
-        "http://localhost:8250/oidc/callback",
-    ]
-    role_type       = "oidc"
-    user_claim = "sub"
-    oidc_scopes = [ "groups" ]
-    groups_claim = "groups"
-    token_policies = [ "default" ]
+  backend         = vault_jwt_auth_backend.okta_oidc.path
+  bound_audiences = [var.okta_client_id]
+  role_name       = "vault-role-okta-group-vault-developer"
+  allowed_redirect_uris = [
+    var.okta_redirect_uris,
+    "http://localhost:8250/oidc/callback",
+  ]
+  role_type      = "oidc"
+  user_claim     = "sub"
+  oidc_scopes    = ["groups"]
+  groups_claim   = "groups"
+  token_policies = ["default"]
 }
 
