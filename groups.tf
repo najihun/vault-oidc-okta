32e3e4eea2ed69c9d451500b32779ead00808d51
najihun <jihun.na@hashicorp.com>
2022-07-18 23:24:05 +0900

    multi tenancy

--- groups.tf
+++ groups.tf
@@ -1,8 +1,3 @@
-locals {
-  child_namespaces = toset(var.child_namespaces)
-}
-
-
 // external group for okta developers
 resource "vault_identity_group" "okta-group-vault-developer" {
   name     = "okta-group-vault-developer"
